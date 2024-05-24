ad82fc5b093100d0cdb3eed22ce4ff64e802e6c3
JoseLopezVilchez <jose.lopez@escuelaestech.es>
2024-05-24 12:29:12 +0200

    progreso del dia, a falta de meterle el glide

---
+++ app/src/main/java/com/example/wikifutbol2/ui/fragments/DetallePersonaFragment.kt
@@ -0,0 +1,72 @@
+package com.example.wikifutbol2.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.example.wikifutbol2.R
+import com.example.wikifutbol2.data.models.personas.Persona
+import com.example.wikifutbol2.databinding.DetallepersonaBinding
+import com.example.wikifutbol2.viewmodels.ElViewModel
+
+class DetallePersonaFragment : Fragment() {
+
+    private var _binding : DetallepersonaBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by activityViewModels<ElViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = DetallepersonaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getPersona().observe(viewLifecycleOwner, observer)
+    }
+
+    private val observer = Observer<Persona> {
+        contentHandler(it)
+    }
+
+    private fun contentHandler(persona : Persona) {
+
+        with(binding) {
+
+            /*Glide.with()
+                .load()
+                .into()
+
+            Glide.with()
+                .load()
+                .into()*/
+
+            imagenpersona
+            imagenescudo
+            costal.text = persona.shirtNumber.toString()
+            nombre.text = persona.name
+            posicion.text = persona.position
+            nacionalidad.text = persona.nationality
+            nacimiento.text = persona.dateOfBirth
+            iniciocontrato.text = persona.currentTeam.contract.start
+            fincontrato.text = persona.currentTeam.contract.until
+
+            when(persona.position) {
+                "Goalkeeper" -> iconoposicion.setImageResource(R.drawable.goal)
+                "Defence" -> iconoposicion.setImageResource(R.drawable.shield)
+                "Midfield" -> iconoposicion.setImageResource(R.drawable.midfield)
+                "Offence" -> iconoposicion.setImageResource(R.drawable.sword)
+            }
+        }
+    }
+
+}
