8d8433411068f81c8382420aab08e2e8150999b9
JoseLopezVilchez <jose.lopez@escuelaestech.es>
2024-05-24 12:05:46 +0200

    layout de detallepersona

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,14 +50,9 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation (libs.retrofit)
     implementation (libs.converter.gson)
-<<<<<<< HEAD
     implementation(libs.androidx.swiperefreshlayout)
     implementation (libs.glide)
     implementation (libs.androidx.core.splashscreen)
-
-=======
-    implementation (libs.glide)
->>>>>>> origin/feature/teams
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
